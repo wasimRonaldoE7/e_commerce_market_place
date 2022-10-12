e0a38a93b84ebc1358dd4f8db836a72a3157bcfb
wasim wasim <wasimwasim@wasims-MacBook-Air.local>
2022-10-12 19:27:11 +0530

    added password hashing with bcrypt

--- services/user.js
+++ services/user.js
@@ -1,16 +1,25 @@
 const userModel = require("../database/models/user");
+const bcrypt=require("bcryptjs");
 
 async function createUser(user) {
 	try {
+		validateRegistration(user);
+        user.password = await bcrypt.hash(user.password, 10);
 		const result = await userModel.create(user);
 		console.log(`${result.name} created succesfully`);
 		return `${result.name} created succesfully as ${result.type}`;
 	} catch (e) {
 		console.log(e);
-        throw `User Creation Failed`; 
+		throw `User Creation Failed`;
 	}
-};
+}
 
+function validateRegistration(user) {
+	const {name, type, password} = user;
+	if (!(name && password && type)) {
+		throw "All input is required";
+	}
+}
 
 module.exports = {
 	createUser,
