cd1f171d0f778ec128feca53f298f0998e73bb2c
wasim wasim <wasimwasim@wasims-MacBook-Air.local>
2022-10-12 18:32:14 +0530

    db connection

---
+++ database/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+async function startConnection() {
+	try {
+        const mongoDbURI = "mongodb://127.0.0.1/my_database";
+		await mongoose.connect(mongoDbURI);
+		console.log('DATABASE CONNECTION SUCCESSFULLY');
+	} catch (error) {
+		console.error(error);
+        console.log('DATABASE CONNECTION FAILED');
+		process.exit(1);
+	}
+}
+
+startConnection();
